3db9214f22514c3717750f4bfe6cf07c6ce6b657
techsithgit <hemilp@tuplecorp.com>
2019-03-13 22:39:13 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+export default function App() {
+
+  const initialState = { 
+    count:0
   }
-}
 
-export default App;
+  function reducer(state = initialState, action){
+    switch(action.type){
+      case "increment":
+        return {count: state.count + 1}
+      default:
+        return initialState
+    }
+  } 
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+  return (
+    <>
+  <div>{state.count}</div>
+  <button onClick={() => dispatch({type:'increment'})}>Up</button>
+  </>
+    )
+}
